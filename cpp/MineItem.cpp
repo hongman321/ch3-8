57be54c449f7f7c9b5d596b082d093fd11a17920
hongman321 <goldpig041234@gmail.com>
2025-02-14 09:47:34 +0900

    update 베이스아이템과 지뢰아이템에 파티클 및 사운드 추가(베이스 아이템은 소리랑 파티클 효과가 발동이 안됨ㅠ)

--- cpp/MineItem.cpp
+++ cpp/MineItem.cpp
@@ -1,44 +1,94 @@
 #include "MineItem.h"
 #include "Components/SphereComponent.h"
 #include "Kismet/GameplayStatics.h"
+#include "Particles/ParticleSystemComponent.h"
+
 AMineItem::AMineItem()
 {
-    ExplosionDelay = 5.0f;
-    ExplosionRadius = 300.0f;
-    ExplosionDamage = 30.0f;
-    ItemType = "Mine";
+	ExplosionDelay = 5.0f;
+	ExplosionRadius = 300.0f;
+	ExplosionDamage = 30.0f;
+	ItemType = "Mine";
+	bHasExploded = false;
 
-    ExplosionCollision = CreateDefaultSubobject<USphereComponent>(TEXT("ExplosionCollision"));
-    ExplosionCollision->InitSphereRadius(ExplosionRadius);
-    ExplosionCollision->SetCollisionProfileName(TEXT("OverlapAllDynamic"));
-    ExplosionCollision->SetupAttachment(Scene);
+	ExplosionCollision = CreateDefaultSubobject<USphereComponent>(TEXT("ExplosionCollision"));
+	ExplosionCollision->InitSphereRadius(ExplosionRadius);
+	ExplosionCollision->SetCollisionProfileName(TEXT("OverlapAllDynamic"));
+	ExplosionCollision->SetupAttachment(Scene);
 }
 
 void AMineItem::ActivateItem(AActor* Activator)
 {
-    // 5초 후 폭발 실행
-    GetWorld()->GetTimerManager().SetTimer(ExplosionTimerHandle, this, &AMineItem::Explode, ExplosionDelay);
+	if (bHasExploded) return;
+
+	Super::ActivateItem(Activator);
+
+	GetWorld()->GetTimerManager().SetTimer(
+		ExplosionTimerHandle,
+		this,
+		&AMineItem::Explode,
+		ExplosionDelay,
+		false
+	);
+
+	bHasExploded = true;
 }
 
 void AMineItem::Explode()
 {
-    TArray<AActor*> OverlappingActors;
-    ExplosionCollision->GetOverlappingActors(OverlappingActors);
+	UParticleSystemComponent* Particle = nullptr;
 
-    for (AActor* Actor : OverlappingActors)
-    {
-        if (Actor && Actor->ActorHasTag("Player"))
-        {
-            UGameplayStatics::ApplyDamage(
-                Actor,                      // 데미지를 받을 액터
-                ExplosionDamage,            // 데미지 양
-                nullptr,                    // 데미지를 유발한 주체 (지뢰를 설치한 캐릭터가 없으므로 nullptr)
-                this,                       // 데미지를 유발한 오브젝트(지뢰)
-                UDamageType::StaticClass()  // 기본 데미지 유형
-            );
-        }
-    }
+	if (ExplosionParticle)
+	{
+		Particle = UGameplayStatics::SpawnEmitterAtLocation(
+			GetWorld(),
+			ExplosionParticle,
+			GetActorLocation(),
+			GetActorRotation(),
+			false
+		);
+	}
 
-    // 지뢰 제거
-    DestroyItem();
+	if (ExplosionSound)
+	{
+		UGameplayStatics::PlaySoundAtLocation(
+			GetWorld(),
+			ExplosionSound,
+			GetActorLocation()
+		);
+	}
+
+	TArray<AActor*> OverlappingActors;
+	ExplosionCollision->GetOverlappingActors(OverlappingActors);
+
+	for (AActor* Actor : OverlappingActors)
+	{
+		if (Actor && Actor->ActorHasTag("Player"))
+		{
+			UGameplayStatics::ApplyDamage(
+				Actor,
+				ExplosionDamage,
+				nullptr,
+				this,
+				UDamageType::StaticClass()
+			);
+		}
+	}
+
+	DestroyItem();
+
+	if (Particle)
+	{
+		FTimerHandle DestroyParticleTimerHandle;
+
+		GetWorld()->GetTimerManager().SetTimer(
+			DestroyParticleTimerHandle,
+			[Particle]()
+		{
+			Particle->DestroyComponent();
+		},
+			5.0f,
+			false
+		);
+	}
 }
